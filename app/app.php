625eb9fa3380205353b4a09d45624b5e5f80f153
Leah Sherrell and Maggie Harrington <leah@>
2017-02-07 16:56:15 -0800

    clean up root paths, change to check if tasks exist before displaying

--- app/app.php
+++ app/app.php
@@ -13,8 +13,17 @@
     $app->get("/", function() {
         $output = "";
 
-        foreach (Task::getAll() as $task) {
-            $output = $output . "<p>" . $task->getDescription() . "</p>";
+        $all_tasks = Task::getAll();
+
+        if (!empty($all_tasks)) {
+            $output = $output . "
+                <h1>To Do List</h1>
+                <p>Here are all your tasks:</p>
+                ";
+            foreach ($all_tasks as $task) {
+                $output = $output . "<p>" . $task->getDescription() . "</p>";
+            }
+
         }
 
         $output = $output . "
