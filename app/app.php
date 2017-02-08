5a9d93e117094c0a8b10f94dd70b39d8cbd4787c
Leah Sherrell and Maggie Harrington <leah@>
2017-02-07 16:46:02 -0800

    add form to create new tasks when submit button is pressed, returns message

--- app/app.php
+++ app/app.php
@@ -17,7 +17,26 @@
             $output = $output . "<p>" . $task->getDescription() . "</p>";
         }
 
+        $output = $output . "
+        <form action='/tasks' method='post'>
+            <label for='description'>Task Description</label>
+            <input id='description' name='description' type='text'>
+
+            <button type='submit'>Add task</button>
+        </form>
+    ";
+
         return $output;
+    });
+
+    $app->post("/tasks", function() {
+        $task = new Task($_POST['description']);
+        $task->save();
+        return "
+            <h1>You created a task!</h1>
+            <p>" . $task->getDescription() . "</p>
+            <p><a href='/'>View your list of things to do.</a></p>
+        ";
     });
 
     return $app;
