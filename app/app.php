f2bffcdacdd8c9e2a0868bf3ce8e874f02fbbd17
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-21 13:34:39 -0800

    move form and list of categories from categories to index view

--- app/app.php
+++ app/app.php
@@ -23,7 +23,7 @@
 
     $app->get("/", function() use ($app) {
 
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->get("/tasks", function() use ($app) {
@@ -48,7 +48,7 @@
     $app->post("/categories", function() use ($app) {
         $category = new Category($_POST['name']);
         $category->save();
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/delete_categories", function() use ($app) {
