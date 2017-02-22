8b8f5bb4bf3451c50bf4d6e2d1c1d82e4774aba8
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-21 16:23:13 -0800

    add due_date to routes and views

--- app/app.php
+++ app/app.php
@@ -37,7 +37,8 @@
     $app->post("/tasks", function() use($app) {
         $description = $_POST['description'];
         $category_id = $_POST['category_id'];
-        $task = new Task($description, $id = null, $category_id);
+        $due_date = $_POST['due_date'];
+        $task = new Task($description, $id = null, $category_id, $due_date);
         $task->save();
         $category = Category::find($category_id);
         return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
@@ -45,7 +46,7 @@
 
     $app->post("/delete_tasks", function() use($app) {
         Task::deleteAll();
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->post("/categories", function() use ($app) {
